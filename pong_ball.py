804bc0613cdf230034b740bc234414247b3a9896
nnastaran83 <nnastaran83@gmail.com>
2022-08-11 23:21:16 +0300

    Black And White

---
+++ pong_ball.py
@@ -0,0 +1,13 @@
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
+from kivy.uix.widget import Widget
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
